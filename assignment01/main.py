cd05f0bf03e14fe2780d2b7f4ee08acf1b6a6664
Aleksandre Khokhiashvili <alkhok18@freeuni.edu.ge>
2025-03-19 03:08:17 +0400

    run linter + add types

--- assignment01/main.py
+++ assignment01/main.py
@@ -1,20 +1,15 @@
-
-n_sequence = int(input('Input number of strings:'))
-sequences = []
+n_sequence: int = int(input("Input number of strings:"))
+sequences: list[str] = []
 for i in range(n_sequence):
-    sequences.append(input(f'Input string #{i}: ').strip())
+    sequences.append(input(f"Input string #{i}: ").strip())
 
-new_sequence = input(f'Input the target string: ').strip()
+new_sequence: str = input("Input the target string: ").strip()
 
-best = (-1, None)
+best: tuple[int, str] = (-1, "N/A")
 for i, seq in enumerate(sequences):
-    c = 0
-    for (a,b) in zip(seq, new_sequence):
-        c += a==b
+    c: int = 0
+    for a, b in zip(seq, new_sequence):
+        c += a == b
     best = max(best, (c, seq))
 
 print(f'Most similar sequence with {best[0]} characters matching is "{best[1]}"')
-
-
-
-
